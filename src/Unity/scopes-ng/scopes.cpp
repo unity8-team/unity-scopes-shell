9891c3fef031c241ce0502fbbeb2e1e58b8c4f13
Michal Hruby <michal.mhr@gmail.com>
2014-02-20 10:28:01 +0000

    Invalidate aggregator scopes after receiving mediascanner invalidation

--- src/Unity/scopes-ng/scopes.cpp
+++ src/Unity/scopes-ng/scopes.cpp
@@ -196,6 +196,13 @@ void Scopes::refreshFinished()
 
 void Scopes::invalidateScopeResults(QString const& scopeName)
 {
+    // HACK! mediascanner invalidates local media scopes, but those are aggregated, so let's "forward" the call
+    if (scopeName == "mediascanner-music") {
+        invalidateResults("musicaggregator");
+    } else if (scopeName == "mediascanner-video") {
+        invalidateResults("videoaggregator");
+    }
+
     Scope* scope = getScopeById(scopeName);
     if (scope == nullptr) return;
 
