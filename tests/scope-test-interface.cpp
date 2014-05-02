202ecfa04b1e59a7126fbd9dc5548ef1a17de7bd
Pete Woods <pete.woods@canonical.com>
2014-05-02 16:30:56 +0100

    Fix copyright headers

--- tests/scope-test-interface.cpp
+++ tests/scope-test-interface.cpp
@@ -1,8 +1,19 @@
 /*
- * scope-test-interface.cpp
+ * Copyright (C) 2014 Canonical Ltd
  *
- *  Created on: 30 Apr 2014
- *      Author: pete
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Pete Woods <pete.woods@canonical.com>
  */
 
 #include <tests/scope-test-interface.h>
