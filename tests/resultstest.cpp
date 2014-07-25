d7631d4d3630e7607357ebd8a452874719678d68
Michal Hruby <michal.mhr@gmail.com>
2014-07-25 13:41:08 +0100

    Don't let the exception from search_hint() kill unity

--- tests/resultstest.cpp
+++ tests/resultstest.cpp
@@ -190,6 +190,15 @@ private Q_SLOTS:
         QCOMPARE(m_scope->isActive(), false);
         m_scope->setActive(true);
         QCOMPARE(m_scope->isActive(), true);
+
+        QCOMPARE(m_scope_ttl->id(), QString("mock-scope-ttl"));
+        QCOMPARE(m_scope_ttl->name(), QString("mock-ttl.DisplayName"));
+        QCOMPARE(m_scope_ttl->iconHint(), QString("/mock-ttl.Icon"));
+        QCOMPARE(m_scope_ttl->description(), QString("mock-ttl.Description"));
+        QCOMPARE(m_scope_ttl->searchHint(), QString());
+        QCOMPARE(m_scope_ttl->shortcut(), QString());
+        QCOMPARE(m_scope_ttl->visible(), true);
+        QCOMPARE(m_scope_ttl->searchQuery(), QString());
     }
 
     void testCategoryQuery()
