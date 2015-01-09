d7584a51369833d5045694776afe43cc147e9908
Pawel Stolowski <pawel.stolowski@canonical.com>
2015-01-09 17:10:33 +0100

    Refactored sample test case using unittest framework and ScopeHarnessTestCase.

--- src/python/pyscope_harness/testing/testcase.py
+++ src/python/pyscope_harness/testing/testcase.py
@@ -1,6 +1,15 @@
 from unittest import TestCase
+from pyscope_harness import MatchResult
 
 class ScopeHarnessTestCase (TestCase):
-    def assertMatchResult(match_result):
-        self.assertIsInstance(match_result, pyscope_harness.MatchResult, msg='match_result must be an instance of MatchResult')
+    """ A class whose instances are single test cases.
+
+    This class extends unittest.TestCase with helper methods relevant for testing of Unity scopes.
+    """
+
+    def assertMatchResult(self, match_result):
+        """ Assert for MatchResult object that fails if match wasn't successful and prints
+            conditions which were not met by the matcher.
+        """
+        self.assertIsInstance(match_result, MatchResult, msg='match_result must be an instance of MatchResult')
         self.assertTrue(match_result.success, msg=match_result.concat_failures)
--- src/python/python-test.py
+++ src/python/python-test.py
@@ -5,33 +5,36 @@ import unittest
 
 TEST_DATA_DIR='/home/vivid/python-harness/tests/data/'
 
-harness = ScopeHarness.new_from_scope_list(Parameters([
+class SimpleResultsTest (ScopeHarnessTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.harness = ScopeHarness.new_from_scope_list(Parameters([
             TEST_DATA_DIR + "mock-scope/mock-scope.ini",
             TEST_DATA_DIR + "mock-scope-info/mock-scope-info.ini",
             TEST_DATA_DIR + "mock-scope-ttl/mock-scope-ttl.ini"
             ]))
-view = harness.results_view
-view.set_active_scope("mock-scope")
-view.search_query = "minimal"
-#view.wait_for_results_change()
+        cls.view = cls.harness.results_view
+        cls.view.set_active_scope("mock-scope")
+        cls.view.search_query = "minimal"
+        #cls.view.wait_for_results_change()
 
-print("TEST 1...")
+    def test_1(self):
+        match = CategoryListMatcher().has_at_least(1).match(self.view.categories)
+        self.assertMatchResult(match)
 
-matchres = CategoryListMatcher().has_at_least(1).match(view.categories)
-print(matchres.success)
-print(matchres.concat_failures)
-
-print("TEST 2...")
-match2 = CategoryListMatcher() \
-    .has_at_least(1) \
-    .mode(CategoryListMatcherMode.BY_ID) \
-    .category(CategoryMatcher("cat1") \
+    def test_2(self):
+        match = CategoryListMatcher() \
             .has_at_least(1) \
-            .mode(CategoryMatcherMode.BY_URI) \
-            .result(ResultMatcher("test:uri") \
-            .properties({'title': 'result for: "minimal"'}) \
-            .art("") \
-            )) \
-    .match(view.categories)
-print(match2.success)
-print(match2.concat_failures)
+            .mode(CategoryListMatcherMode.BY_ID) \
+            .category(CategoryMatcher("cat1") \
+                    .has_at_least(1) \
+                    .mode(CategoryMatcherMode.BY_URI) \
+                    .result(ResultMatcher("test:uri") \
+                    .properties({'title': 'result for: "minimal"'}) \
+                    .art("") \
+                    )) \
+            .match(self.view.categories)
+        self.assertMatchResult(match)
+
+if __name__ == '__main__':
+    unittest.main()
