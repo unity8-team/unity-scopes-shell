85be405fa10ef45fc7ae2dd3ab08a8a41df48486
Pawel Stolowski <pawel.stolowski@canonical.com>
2015-09-23 17:35:40 +0200

    Remove the workaround with keeping reference to self

--- src/Unity/scope.cpp
+++ src/Unity/scope.cpp
@@ -1253,11 +1253,6 @@ void Scope::activate(QVariant const& result_var, QString const& categoryId)
 
 unity::shell::scopes::PreviewStackInterface* Scope::preview(QVariant const& result_var, QString const& categoryId)
 {
-    // Keep reference to self if we're a temp scope, or otherwise we will crash in loginToAccount() if closeScope()
-    // is called - see LP: #1410191
-    auto self = findTempScope(id());
-    Q_UNUSED(self);
-
     if (!result_var.canConvert<std::shared_ptr<scopes::Result>>()) {
         qWarning("Cannot preview, unable to convert %s to Result", result_var.typeName());
         return nullptr;
