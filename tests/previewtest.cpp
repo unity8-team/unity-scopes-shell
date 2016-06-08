fd39de7b6db2fe4cb12ec925a163224f74b35a98
Pawel Stolowski <pawel.stolowski@canonical.com>
2016-06-08 11:19:48 +0200

    Add test case

--- tests/previewtest.cpp
+++ tests/previewtest.cpp
@@ -651,6 +651,46 @@ private Q_SLOTS:
             .match(previewView2->widgets())
             );
     }
+
+    void testPreviewReplacingPreviewWithoutLayouts()
+    {
+        m_resultsView->setQuery("preview-replace-no-layouts");
+
+        auto abstractView = m_resultsView->category(0).result(0).longPress();
+        QVERIFY(bool(abstractView));
+        auto previewView = dynamic_pointer_cast<shv::PreviewView>(abstractView);
+        QVERIFY(bool(previewView));
+
+        QVERIFY_MATCHRESULT(
+            shm::PreviewColumnMatcher()
+            .column(
+                shm::PreviewMatcher()
+                .widget(shm::PreviewWidgetMatcher("img"))
+                .widget(shm::PreviewWidgetMatcher("hdr"))
+                .widget(shm::PreviewWidgetMatcher("desc"))
+                .widget(shm::PreviewWidgetMatcher("actions"))
+            )
+            .match(previewView->widgets())
+        );
+
+        sc::VariantMap hints {{"session-id", sc::Variant("goo")}};
+        auto sameView = previewView->widgetsInFirstColumn().at("actions").trigger("download", sc::Variant(hints));
+        auto previewView2 = dynamic_pointer_cast<shv::PreviewView>(sameView);
+        QVERIFY(bool(previewView2));
+
+        // the order of widgets shouldn't change
+        QVERIFY_MATCHRESULT(
+            shm::PreviewColumnMatcher()
+            .column(
+                shm::PreviewMatcher()
+                .widget(shm::PreviewWidgetMatcher("img"))
+                .widget(shm::PreviewWidgetMatcher("hdr"))
+                .widget(shm::PreviewWidgetMatcher("desc"))
+                .widget(shm::PreviewWidgetMatcher("actions"))
+            )
+            .match(previewView2->widgets())
+        );
+    }
 };
 
 QTEST_GUILESS_MAIN(PreviewTest)
